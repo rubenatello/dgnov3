8d956833d066752c5e1ee76119b99009162ece83
Ruben Cazarez <rcazarez@sunroadent.com>
2025-10-21 10:27:28 -0700

    Adding proper import for Admin Icon...

--- app/src/components/DashboardOverlayButton.tsx
+++ app/src/components/DashboardOverlayButton.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from '../hooks/useAuth';
-import adminIcon from '../../assets/adminicon.png';
+import adminIcon from '../../public/icons/admin-icon.png';
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
